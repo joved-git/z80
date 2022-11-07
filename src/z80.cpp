5dc1635628dcc415d4323587ae918f50736f1ba2
jvedrenne <jvedrenne@e3541d03>
2022-11-07 09:27:20 +0100

    Add 'h' command.

--- src/z80.cpp
+++ src/z80.cpp
@@ -5,7 +5,8 @@ int main(int argc, char *argv[])
     bool exit=false;
     char c;
 
-    std::cout << "Hello Z80 world !" << std::endl;
+    std::cout << "Hello Z80 world !" << std::endl << std::endl;
+    std::cout << "Type h if your want some help..." << std::endl << std::endl;
 
     while (!exit)
     {
@@ -15,6 +16,15 @@ int main(int argc, char *argv[])
         if (c=='x')
         {
             exit=true;
+        }
+
+        if (c=='h')
+        {
+            printf("a <code>: translate <code> in command\n");
+            printf("m <cmd>: translate <cmd> in machine code\n");
+            printf("r: display registries\n");
+            printf("\n");
+            printf("<cmd>: execute the command\n");
         }
     }
 
