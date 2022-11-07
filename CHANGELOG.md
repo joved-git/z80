a987120abcceb00146b263c009888a226e2dceb2
jvedrenne <jvedrenne@e3541d03>
2022-11-07 10:03:44 +0100

    Add the 'z>' prompt. Add the 'x' command in the help.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,13 @@
+# 0.0.4
+Add the 'z>' prompt.
+Add the 'x' command in the help.
+
 # 0.0.3
 Add a CHANGELOG.md file.
 Add a version.hpp file. Display the version.
 
 # 0.0.2:
-Add a h command.
+Add a 'h' command.
 
 # 0.0.1: 
 First version with almost nothing.
--- src/z80.cpp
+++ src/z80.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "../inc/version.hpp"
 
+#define EOL '\n'
+
 int main(int argc, char *argv[])
 {
     bool exit=false;
@@ -9,11 +11,12 @@ int main(int argc, char *argv[])
     std::cout << "Hello Z80 world !" << std::endl;
     std::cout << "Version: " << VERSION << std::endl << std::endl;
     std::cout << "Type h if your want some help..." << std::endl << std::endl;
+    std::cout << "z> ";
 
     while (!exit)
     {
         c=getchar();
-        //printf("%c", c);
+        /* printf("%d-", c);    */
 
         if (c=='x')
         {
@@ -22,11 +25,18 @@ int main(int argc, char *argv[])
 
         if (c=='h')
         {
-            printf("a <code>: translate <code> in command\n");
+            printf("\na <code>: translate <code> in assembly langage\n");
             printf("m <cmd>: translate <cmd> in machine code\n");
             printf("r: display registries\n");
+            printf("x: exit me\n");
             printf("\n");
             printf("<cmd>: execute the command\n");
+            printf("<code>: execute the code\n\n");
+        }
+
+        if (c==EOL)
+        {
+            std::cout << "z> ";
         }
     }
 
