11c9075fbcec0553a087df09100d83cec3b00b45
joved@hebus <joe@smile.fr>
2025-02-12 19:41:16 +0100

    Remove tabiulations from jpcc.asm file (must be done with all files)

--- codefile/jpcc.asm
+++ codefile/jpcc.asm
@@ -1,13 +1,13 @@
 org #0480
 
-init:	ld a,#FE
-loop:	add a,#1
-		nop
-		nop
-		jp nc,loop
+init:   ld a,#FE
+loop:   add a,#1
+        nop
+        nop
+        jp nc,loop
 
-		ld a,#FC
-		jp init
-		nop
-		halt
+        ld a,#FC
+        jp init
+        nop
+        halt
 end
